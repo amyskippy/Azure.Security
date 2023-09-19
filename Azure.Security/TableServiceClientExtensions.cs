63494ab0effa6b7b445d31bca78d8d2300c082d9
Amy Hesketh <amy@professionaloffice.co.uk>
2023-09-19 20:36:30 +0100

    Added extension methods for detecting whether a table exists

---
+++ Azure.Security/TableServiceClientExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Azure.Security
+{
+    using Data.Tables;
+    using System.Threading.Tasks;
+
+    public static class TableServiceClientExtensions
+    {
+        public static bool Exists(this TableServiceClient client, string tableName)
+        {
+            var exists = false;
+            foreach (var tbl in client.Query(t => t.Name == tableName))
+            {
+                exists = true;
+            }
+            return exists;
+        }
+
+        public static async Task<bool> ExistsAsync(this TableServiceClient client, string tableName)
+        {
+            var exists = false;
+            await foreach (var tbl in client.QueryAsync(t => t.Name == tableName))
+            {
+                exists = true;
+            }
+            return exists;
+        }
+    }
+}
